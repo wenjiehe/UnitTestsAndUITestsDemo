4de98bec16d8ed75e386f9b62cec08d907a4b614
heweiqi123 <123610105@qq.com>
2020-01-19 01:13:46 +0800

    Initial Commit

---
+++ UnitTestsAndUITestsDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  UnitTestsAndUITestsDemo
+//
+//  Created by 贺文杰 on 2020/1/19.
+//  Copyright © 2020 贺文杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ UnitTestsAndUITestsDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UnitTestsAndUITestsDemo
+//
+//  Created by 贺文杰 on 2020/1/19.
+//  Copyright © 2020 贺文杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
