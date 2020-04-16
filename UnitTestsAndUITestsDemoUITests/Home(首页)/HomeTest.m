2dc49ea8a19c48a76db29a6ea32e5dc18666c269
hewenjie <123610105@qq.com>
2020-04-16 23:59:37 +0800

    。。

--- UnitTestsAndUITestsDemoUITests/Home(首页)/HomeTest.m
+++ UnitTestsAndUITestsDemoUITests/Home(首页)/HomeTest.m
@@ -14,6 +14,8 @@
 
 @implementation HomeTest
 
+//如果运行整个测试模块，执行顺序为 1.setUp 2.testTap 3.tearDown
+
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
     
@@ -21,8 +23,11 @@
     self.continueAfterFailure = NO;
 
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
-    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch]; //启动app
+    //查看你app的视图层级
+    NSLog(@"debugDescription = %@", [app debugDescription]);
+    NSLog(@"%s", __FUNCTION__);
 
 
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
@@ -30,6 +35,11 @@
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    NSLog(@"%s", __FUNCTION__);
+}
+
+- (void)testTap{
+    NSLog(@"%s", __FUNCTION__);
 }
 
 - (void)testExample {
@@ -37,56 +47,56 @@
             // Use XCTAssert and related functions to verify your tests produce the correct results.
 //    [[[[[[XCUIApplication alloc] init].tables.collectionViews childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element tap];
     //https://www.jianshu.com/p/cf445a2f5f5b
-    XCUIApplication *app = [XCUIApplication new];
-    [app launch]; //启动app
-    [app activate]; //激活app:如果app进入后台，可以用这个方法激活启动到前台
-    [app terminate]; //终止所有app
-
-    switch ([app state]) {
-        case XCUIApplicationStateUnknown:
-            //未知状态
-            break;
-        case XCUIApplicationStateNotRunning:
-            //未运行
-            break;
-        case XCUIApplicationStateRunningBackgroundSuspended:
-            //后台运行但被挂起
-            break;
-        case XCUIApplicationStateRunningBackground:
-            //后台运行
-            break;
-        case XCUIApplicationStateRunningForeground:
-            //正在前台运行
-            break;
-        default:
-            break;
-    }
+//    XCUIApplication *app = [XCUIApplication new];
+//    [app launch]; //启动app
+//    [app activate]; //激活app:如果app进入后台，可以用这个方法激活启动到前台
+//    [app terminate]; //终止所有app
+//
+//    switch ([app state]) {
+//        case XCUIApplicationStateUnknown:
+//            //未知状态
+//            break;
+//        case XCUIApplicationStateNotRunning:
+//            //未运行
+//            break;
+//        case XCUIApplicationStateRunningBackgroundSuspended:
+//            //后台运行但被挂起
+//            break;
+//        case XCUIApplicationStateRunningBackground:
+//            //后台运行
+//            break;
+//        case XCUIApplicationStateRunningForeground:
+//            //正在前台运行
+//            break;
+//        default:
+//            break;
+//    }
     
-    //查询类型
-    XCUIElementQuery *buttons = [app descendantsMatchingType:XCUIElementTypeButton];
-    
-    if (buttons.count > 0) {
-        XCUIElement *button = [buttons elementBoundByIndex:0];
-        if (button.exists) {//确定元素是否存在
-            [button tap]; //单个点击事件
-            [button doubleTap]; //双击事件
-//            button.hittable; //是否可以计算元素的命中点
-//            button.debugDescription; //提供有关元素的调试信息
-            [button typeText:@""]; //在元素中输入一个字符串
-            [button pressForDuration:3]; //发送长按手势，并指定时间
-            [button pressForDuration:3 thenDragToElement:button]; //发送长按手势，并指定时间
-            
-            [button swipeLeft]; //
-        }
-    }
+//    //查询类型
+//    XCUIElementQuery *buttons = [app descendantsMatchingType:XCUIElementTypeButton];
+//
+//    if (buttons.count > 0) {
+//        XCUIElement *button = [buttons elementBoundByIndex:0];
+//        if (button.exists) {//确定元素是否存在
+//            [button tap]; //单个点击事件
+//            [button doubleTap]; //双击事件
+////            button.hittable; //是否可以计算元素的命中点
+////            button.debugDescription; //提供有关元素的调试信息
+//            [button typeText:@""]; //在元素中输入一个字符串
+//            [button pressForDuration:3]; //发送长按手势，并指定时间
+//            [button pressForDuration:3 thenDragToElement:button]; //发送长按手势，并指定时间
+//
+//            [button swipeLeft]; //
+//        }
+//    }
     
 
 
-    XCUIElementQuery *tablesQuery = [[XCUIApplication alloc] init].tables;
-    NSInteger count = tablesQuery.cells.count;
-    if (count > 0) {
-        [[tablesQuery.cells elementBoundByIndex:0] tap];
-    }
+//    XCUIElementQuery *tablesQuery = [[XCUIApplication alloc] init].tables;
+//    NSInteger count = tablesQuery.cells.count;
+//    if (count > 0) {
+//        [[tablesQuery.cells elementBoundByIndex:0] tap];
+//    }
 
 //    XCUIElement *cell = [[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:0];
 //    [cell tap];
